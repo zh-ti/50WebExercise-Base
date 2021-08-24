cb868193e30ad3eff1f38df9c9414a69468da486
Tian <2755409448@qq.com>
2021-08-24 17:16:22 +0800

    master

--- 19-theme-clock/script.js
+++ 19-theme-clock/script.js
@@ -1,114 +1,122 @@
-window.onload = function(){
-    const body = document.querySelector("body");
-    const modeBtn = document.querySelector(".mode");
-    const [hour,,minute,half] = document.querySelectorAll("h2 span");
-    const [week, month, day] = document.querySelectorAll("p span");
-    const [hourPointer, minutePointer, secoundPointer] = document.querySelectorAll(".clock > div");
-    let secondRound = minuteRound = hourRound = 0;
-    let minuteTime = hourTime = null;
+window.onload = function () {
+    const body = document.querySelector('body');
+    const modeBtn = document.querySelector('.mode');
+    const [hour, , minute, half] = document.querySelectorAll('h2 span');
+    const [week, month, day] = document.querySelectorAll('p span');
+    const [hourPointer, minutePointer, secoundPointer] = document.querySelectorAll('.clock > div');
+    let secondRound = (minuteRound = hourRound = 0);
+    let minuteTime = (hourTime = null);
 
-    function MyDate(date){
+    function MyDate(date) {
         return {
             hours: date.getHours(),
             minutes: date.getMinutes(),
             seconds: date.getSeconds(),
             day: date.getDate(),
-            getWeek: function(){
-                switch(date.getDay()){
+            getWeek: function () {
+                switch (date.getDay()) {
                     case 0:
-                        return "Sunday";
+                        return 'Sunday';
                     case 1:
-                        return "Monday";
+                        return 'Monday';
                     case 2:
-                        return "Tuesday";
+                        return 'Tuesday';
                     case 3:
-                        return "Wednesday";
+                        return 'Wednesday';
                     case 4:
-                        return "Thursday"
+                        return 'Thursday';
                     case 5:
-                        return "Friday";
+                        return 'Friday';
                     case 6:
-                        return "Saturday";
+                        return 'Saturday';
                 }
             },
-            getMonth: function(){
-                switch(date.getMonth()){
+            getMonth: function () {
+                switch (date.getMonth()) {
                     case 0:
-                        return "JAN";
+                        return 'JAN';
                     case 1:
-                        return "FEB";
+                        return 'FEB';
                     case 2:
-                        return "MAR";
+                        return 'MAR';
                     case 3:
-                        return "APR";
+                        return 'APR';
                     case 4:
-                        return "MAY";
+                        return 'MAY';
                     case 5:
-                        return "JUN";
+                        return 'JUN';
                     case 6:
-                        return "JUL";
+                        return 'JUL';
                     case 7:
-                        return  "AUG";
+                        return 'AUG';
                     case 8:
-                        return "SEP";
+                        return 'SEP';
                     case 9:
-                        return "OCT";
+                        return 'OCT';
                     case 10:
-                        return "NOV";
+                        return 'NOV';
                     case 11:
-                        return "DEC";
+                        return 'DEC';
                 }
-            }   
-        }
+            },
+        };
     }
 
     init();
-    function init(){
+    function init() {
         //添加模式切换功能
-        modeBtn.addEventListener("click", ()=>{
-            body.classList.toggle("dark-mode");
-            if(body.classList.value.indexOf("dark-mode") != -1){
-                modeBtn.innerText = "Light Mode";
-            }else{
-                modeBtn.innerText = "Dark Mode";
+        modeBtn.addEventListener('click', () => {
+            body.classList.toggle('dark-mode');
+            if (body.classList.value.indexOf('dark-mode') != -1) {
+                modeBtn.innerText = 'Light Mode';
+            } else {
+                modeBtn.innerText = 'Dark Mode';
             }
         });
-        setInterval(()=>start(), 1000); //每秒指针跳动
+        setInterval(() => start(), 1000); //每秒指针跳动
     }
 
-    function start(){
+    function start() {
         const date = new MyDate(new Date());
-        function setInfo(){
+        function setInfo() {
             //设置日期时间信息
             hour.innerText = date.hours;
-            date.minutes < 10 ? minute.innerText = "0" + date.minutes : minute.innerText = date.minutes;
-            date.hours > 12 ? half.innerText = "PM" : half.innerText = "AM";
+            date.minutes < 10
+                ? (minute.innerText = '0' + date.minutes)
+                : (minute.innerText = date.minutes);
+            date.hours > 12 ? (half.innerText = 'PM') : (half.innerText = 'AM');
             week.innerText = date.getWeek(date.date);
             month.innerText = date.getMonth(date.date);
             day.innerText = date.day;
         }
-        function clockLaunch(){
-            360/60*date.seconds  == 0 && secondRound++; //秒针走完一圈 secoundRound++
+        function clockLaunch() {
+            (360 / 60) * date.seconds == 0 && secondRound++; //秒针走完一圈 secoundRound++
             // 秒针走完一圈且 minuteTime 不存在 或者 当前的时间减去 minuteTime 大于等于 60s 时，minuteRound++
-            const time = Date.now()
+            const time = Date.now();
             const judge = time - minuteTime >= 6000;
-            if(360/60*date.minutes  == 0 && (!minuteTime || judge)){
-                miniteRound++;
+            if ((360 / 60) * date.minutes == 0 && (!minuteTime || judge)) {
+                minuteRound++;
                 minuteTime = Date.now();
             }
             // 秒针走完一圈且 hourTime 不存在 或者 当前的时间减去 hourTime 大于等于 60m 时，hourRound++
-            if(360/60*date.hours  == 0 && (!hourTime || Date.now() - hourTime >= 60000)){
+            if ((360 / 60) * date.hours == 0 && (!hourTime || Date.now() - hourTime >= 60000)) {
                 hourRound++;
                 hourTime = Date.now();
             }
-            if(360/60*date.minutes  == 0){
-                console.log("Date.now()", time, minuteTime, judge);
+            if ((360 / 60) * date.minutes == 0) {
+                console.log('Date.now()', time, minuteTime, judge);
             }
-            secoundPointer.style.transform = `rotate(${360/60*date.seconds + (secondRound * 360)}deg)`;
-            minutePointer.style.transform = `rotate(${360/60*date.minutes + (minuteRound * 360) + (360/3600*date.seconds)}deg)`;
-            hourPointer.style.transform = `rotate(${360/12*date.hours + (minuteRound * 360)+(360/720*date.minutes)}deg)`;
+            secoundPointer.style.transform = `rotate(${
+                (360 / 60) * date.seconds + secondRound * 360
+            }deg)`;
+            minutePointer.style.transform = `rotate(${
+                (360 / 60) * date.minutes + minuteRound * 360 + (360 / 3600) * date.seconds
+            }deg)`;
+            hourPointer.style.transform = `rotate(${
+                (360 / 12) * date.hours + minuteRound * 360 + (360 / 720) * date.minutes
+            }deg)`;
         }
         setInfo();
         clockLaunch();
     }
-}
+};
